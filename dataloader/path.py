f8d9bfeb4a1fbdc06b99a2aee8b14e53093730e0
NeverMoes <nevermoes@gmail.com>
2017-04-28 16:43:05 +0800

    path fixed

--- dataloader/path.py
+++ dataloader/path.py
@@ -3,3 +3,5 @@ CIFAR10 = '/Users/nemos/dataset/common/cifar-10-batches-bin'
 
 TITANIC = '/Users/nemos/dataset/kaggle/titanic'
 IMDB = '/Users/nemos/dataset/kaggle/IMDB'
+
+NLTK_DATA = '/Users/nemos/dataset/nltk_data'
